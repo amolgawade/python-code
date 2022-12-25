b6ce0bcad5a51ee9d3297d8da1b1aed7dcd0409d
Amol <amolsg863@gmail.com>
2022-12-25 20:41:19 +0530

    new class fromation for study

---
+++ src/calculator.py
@@ -0,0 +1,5 @@
+from calculator_def import *
+
+print(add(5, 10))
+print(subtraction(20, 7))
+print(percentage(24, 30))
---
+++ src/calculator_def.py
@@ -0,0 +1,11 @@
+
+def add(a, b):
+    return a+b
+
+
+def subtraction(a, b):
+    return a-b
+
+
+def percentage(a, b):
+    return str((a/b)*100) + "%"
