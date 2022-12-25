f85ba67d633851e3caf1b37481fb89f21240b4d5
Amol <amolsg863@gmail.com>
2022-12-25 12:35:58 +0530

    practise

---
+++ src/first.py
@@ -0,0 +1,41 @@
+print('amol')
+
+print("\tamol")
+
+print("amol\ngawade\nbaramati")
+print("name=amol gawde\naddress\n\tbaramati\n\tpragtinagr\n\t devkate niwas")
+
+favorite_language = '   python   '
+x= favorite_language.rstrip()
+print(x)
+
+
+txt= "  mangoes   "
+x= txt.strip()
+print ('out of all fruits',x,'is my favourate')
+
+who= "there are two cat's"
+print(who)
+
+person_name1="Amol Gawade"
+person_name2="Shvaji Gawade"
+print(person_name1.upper(),person_name2.title())
+
+famous_person="albert einstine"
+famous_Quote= "A person who never made a mistake never tried anything new."
+txt=famous_person+'\n\t' +famous_Quote
+print("This is most famous line by brillent scientis : ",txt.upper(),"He was the one who invented the "
+                                                             "E=mc^2 and made grate revoulation to science ")
+
+A1="Mahatma Gandhi"
+Dia1="The greatness of a nation and its moral progress can be judged by the way its animals are treated."
+A2="Pandit Neharu"
+Dia2="बिना शांति के, सभी सपने टूट जाते हैं और राख में मिल जाते हैं"
+A3="Abddul kalam "
+Dia3="You have to dream before your dreams can come true"
+add=A1+'\n\t'+Dia1+'\n'+A2+'\n\t'+Dia2+'\n'+A3+'\n\t'+Dia3
+print("some famous dialouges:",add.title())
+
+
+print(10+20)
+
