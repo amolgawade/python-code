83447bfb2c0005ad5c5de1e3ee2914c7153507ab
Amol <amolsg863@gmail.com>
2022-12-25 19:43:00 +0530

    end of the day 25 dec

--- src/first.py
+++ src/first.py
@@ -37,5 +37,94 @@ add=A1+'\n\t'+Dia1+'\n'+A2+'\n\t'+Dia2+'\n'+A3+'\n\t'+Dia3
 print("some famous dialouges:",add.title())
 
 
-print(10+20)
+print(10+20-(5/10*200/10)+500)
 
+print(0.2*0.2)
+age=23
+message="Happy " + str(age)+'rd birthaday'
+print(message)
+
+print(5+3)
+print(10-2)
+print(2*4)
+print(16/2)
+
+# hi to everybody
+print ("Hi Everybody") # these is greeting
+
+import this
+
+bicycles=['treck','regular','hero','atlas']
+print (bicycles)
+
+bicycles=['treck','regular','hero','atlas']
+print(bicycles[0])
+print(bicycles[0].title())
+print(bicycles[2])
+print(bicycles[1].upper())
+print(bicycles[3].lower())
+print (bicycles[-1])
+print(bicycles[-2])
+print(bicycles[-3])
+
+
+biy=['atlas','hero','herculas','ranger','ladies','thonder','gear']
+print(biy[4].title())
+print(biy)
+message=('My first bicycle is '+ biy[0].upper()+'.'+'i ride along with my friend')
+print(message)
+
+alpha=['a', 'b', 'c', 'd', 'e', 'f','g','h']
+print (alpha[2])
+print(alpha[-2])
+message=('my name start with letter '+alpha[0].upper()+' and my last name start with letter '+alpha[-2].upper())
+print(message)
+print(alpha[0]+alpha[2]+alpha[-1]+alpha[5]+' this is good practice for my study')
+print(message,'.i love my self',' . During training in python i study various methods of oprations ')
+
+alpha=['a', 'b', 'c', 'd', 'e', 'f','g','h']
+print(alpha)
+alpha[1]='x'
+print(alpha)
+alpha[-2]='z'
+print(alpha)
+
+alpha=['a', 'b', 'c', 'd', 'e', 'f','g','h']
+alpha.append('k')
+print(alpha)
+alpha.append('s')
+print(alpha)
+
+gun=[]
+print (gun)
+gun.append('shotgun')
+gun.append('automated')
+gun.append('revolwer')
+gun.append('german')
+gun.append('desi katta')
+gun.append('brahmose')
+print(gun)
+gun.insert(2,'rocket')
+print(gun)
+gun.insert(4,'agni1')
+print(gun)
+
+del gun[1]
+print(gun)
+
+gun=['shotgun', 'rocket', 'revolwer', 'agni1', 'german', 'desi katta', 'brahmose','tank']
+print(gun)
+popp_gun=gun.pop()
+print(gun)
+print(popp_gun)
+gun=['shotgun', 'rocket', 'revolwer', 'agni1', 'german', 'desi katta', 'brahmose','tank']
+last_woned=gun.pop()
+print('The last weapon i hold was '+last_woned.upper()+'.')
+
+third_woned=gun.pop(4)
+print('The third weapon i hold was '+third_woned.upper()+'.')
+gun=['shotgun', 'rocket', 'revolwer', 'agni1', 'german', 'desi katta', 'brahmose','tank']
+print(gun)
+removed_item=gun.remove('desi katta')
+print(gun)
+print(removed_item)
