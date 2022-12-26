a1df3459f7f686d7128a9923e41193bbf732eca5
Amol <amolsg863@gmail.com>
2022-12-26 18:22:36 +0530

    updated time 6:22

--- src/new_day26/12/2022.py
+++ src/new_day26/12/2022.py
@@ -42,7 +42,68 @@ print("These are all weapons are not for sale and use "
 message = 'hello everyone , i m amol gawade'
 print(message)
 
+for value in range(1,5):
+    print(value)
+
+num = list(range(1,6))
+print(num)
+num = list(range(3,30,3))
+print(num)
+
+squares = []
+for value in range(1,11):
+    num = value**2
+    squares.append(num)
+print(squares)
+
+num = list(range(100, 1000, 100))
+print(num)
+print(min(num))
+print(max(num))
+print(sum(num))
+
+num = [value**2 for value in range(2, 21)]
+print(num)
+
+num = list(range(1, 100000))
+print(num)
+
+print(sum(num))
+
+num = []
+for num in range (3, 33):
+    txt = list [ num*2 ]
+    print(txt)
+
+num = list(range(1,6))
+txt = list (num*2)
+print(txt)
+num = list(range(3,30,3))
+print(num)
+
+gun = ['shotgun', 'rocket', 'revolver', 'agni1', 'german', 'desi katta', 'brahmose', 'tank']
+print("here are three weapon that are dangerous")
+for num in gun[0:5]:
+    print(num.title())
 
 
+my = ['toyota','suzuki']
+friend = my[:]
 
+print('my favourite car is: ')
+my.append('hero')
+print(my)
+print("\n my friends favourite car is: ")
+friend.append('honda')
+print(friend)
+
+gun = ['shotgun', 'rocket', 'revolver', 'agni1', 'german', 'desi katta', 'brahmose', 'tank']
+for weapon in gun :
+    if weapon == 'tank':
+        print(weapon.upper())
+    else:
+        print(weapon.title())
+
+car = 'BMW'
+tar = 'and'
 
