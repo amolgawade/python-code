cd7110e1f0bf43715c7eb567489474d563149cf2
Amol <amolsg863@gmail.com>
2022-12-26 14:04:27 +0530

    updated file

---
+++ src/new_day26/12/2022.py
@@ -0,0 +1,48 @@
+
+cars = ['Tata', 'suzuki', 'hoda', 'mahendra', 'kia', 'toyota', 'jeep', 'safari', 'Bmw', 'mercedes']
+cars.sort(reverse=True)
+print(cars)
+
+cars = ['Tata', 'suzuki', 'hoda', 'mahendra', 'kia', 'toyota', 'jeep', 'safari', 'Bmw', 'mercedes']
+print('\nhere is the original list:')
+print(cars)
+print("\nHere is sorted list:")
+print(sorted(cars))
+print('\n\t from these change the code')
+cars = ['Tata', 'suzuki', 'hoda', 'mahendra', 'kia', 'toyota', 'jeep', 'safari', 'Bmw', 'mercedes']
+print(cars)
+cars.reverse()
+print(cars)
+
+cars = ['Tata', 'suzuki', 'hoda', 'mahendra', 'kia', 'toyota', 'jeep', 'safari', 'Bmw', 'mercedes']
+print(len(cars))
+
+gun = ['shotgun', 'rocket', 'revolwer', 'agni1', 'german', 'desi katta', 'brahmose', 'tank']
+print("\n Here is my orignal list of gun:")
+print(gun)
+print("\n Here are now they are sorted :")
+gun.sort()
+print(gun)
+gun.sort(reverse=True)
+print(gun)
+gun.reverse()
+print(gun)
+print(len(gun))
+print(gun[-5])
+
+gun = ['shotgun', 'rocket', 'revolver', 'agni1', 'german', 'desi katta', 'brahmose', 'tank']
+for txt in gun:
+    print(txt.title() + " : This is very dangerous")
+    print("The weapon " +txt.upper()+" , i want to use it "+"\n")
+
+
+print("These are all weapons are not for sale and use "
+          "thanks for asking ")
+
+message = 'hello everyone , i m amol gawade'
+print(message)
+
+
+
+
+
