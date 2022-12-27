9bc28c8aa36d6e7ba10b70f76eb33cfd94853aa3
Amol <amolsg863@gmail.com>
2022-12-27 22:00:35 +0530

    new added

---
+++ src/new_day26/12/if_condition_27/12/dictioneries_1.py
@@ -0,0 +1,15 @@
+dog_1 = {'color':'green','point': 5}
+
+print(dog_1['color'])
+print(dog_1['point'])
+new_point = (dog_1['point'])
+print("you just earned "+str(new_point)+"  points congrats")
+
+dog_1 = {'color':'green','point': 5}
+print(dog_1)
+dog_1['x_position'] = 0
+dog_1['y_position'] = 25
+print(dog_1)
+dog_1['color'] = 'blue'
+print("the new color of dog_1 is "+dog_1['color']+' .')
+print(dog_1)
