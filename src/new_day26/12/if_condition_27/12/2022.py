dc66ef065d2a694b575c08a2099c3feeb08e28b0
Amol <amolsg863@gmail.com>
2022-12-27 10:56:41 +0530

    if condition statment

---
+++ src/new_day26/12/if_condition_27/12/2022.py
@@ -0,0 +1,13 @@
+age = 25
+for age in range(22, 27):
+    if age == 25:
+        print('correct age guess')
+    else:
+        print("not correct age", end ="###")
+
+
+for age in range(18, 22):
+    if age >= 20:
+        print("true")
+    else:
+        print("\t\n\nfalse")
