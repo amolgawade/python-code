428ee6e9ff863b3975d50da241b55bbc34368cbd
Amol <amolsg863@gmail.com>
2022-12-28 23:05:54 +0530

    added at 11.05pm

---
+++ src/day_28/another.py
@@ -0,0 +1,6 @@
+i = input('how tall you are ? , in inches ?')
+height = int(i)
+if height >= 36:
+    print("you are eligible for riding")
+else:
+    print("sorry you are not eligible for riding")
---
+++ src/day_28/even_odd_input.py
@@ -0,0 +1,7 @@
+
+i = input("enter the no i will tell its even or odd")
+number = int(i)
+if number % 2 == 0:
+    print("\n your no is : "+ str(number)+" even")
+else:
+    print("your no is : "+ str(number)+" odd")
